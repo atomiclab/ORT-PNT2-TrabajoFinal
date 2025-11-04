8a3545b4cc22b593e3e3d396f9bc450d58b4fafb
atomiclab <ginotubaro@gmail.com>
2025-11-04 17:31:52 -0300

    paginado

--- src/components/PequenoComercio/src/PequenoComercio.js
+++ src/components/PequenoComercio/src/PequenoComercio.js
@@ -16,11 +16,20 @@ export default {
       usuarios: [],
       cargando: false,
       error: null,
+      paginaActual: 1,
+      usuariosPorPagina: 9,
     }
   },
 
   computed: {
-    // propiedades calculadas
+    totalPaginas() {
+      return Math.ceil(this.usuarios.length / this.usuariosPorPagina)
+    },
+    usuariosPaginados() {
+      const inicio = (this.paginaActual - 1) * this.usuariosPorPagina
+      const fin = inicio + this.usuariosPorPagina
+      return this.usuarios.slice(inicio, fin)
+    },
   },
 
   watch: {
@@ -35,11 +44,27 @@ export default {
         const servicio = new servicioProductos()
         const usuarios = await servicio.getAll()
         this.usuarios = usuarios || []
+        this.paginaActual = 1 
       } catch (error) {
         this.error = 'Error al cargar los usuarios'
         console.error('Error al obtener usuarios:', error)
       } finally {
         this.cargando = false
+      }
+    },
+    irAPagina(pagina) {
+      if (pagina >= 1 && pagina <= this.totalPaginas) {
+        this.paginaActual = pagina
+      }
+    },
+    paginaAnterior() {
+      if (this.paginaActual > 1) {
+        this.paginaActual--
+      }
+    },
+    paginaSiguiente() {
+      if (this.paginaActual < this.totalPaginas) {
+        this.paginaActual++
       }
     },
   },
